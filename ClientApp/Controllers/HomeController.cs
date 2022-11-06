4547f22668b2330b45d2ef53d629c7c3b75d3dcf
envydiace <ninhducanh216@gmail.com>
2022-11-06 10:22:40 +0700

    paging home

--- ClientApp/Controllers/HomeController.cs
+++ ClientApp/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace ClientApp.Controllers
     {
         public string baseUrl = "http://localhost:5000/api/";
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(HomeFilterView filter)
         {
             var categories = await Calculate.GetAllCategory();
             if (categories == null)
@@ -18,19 +18,56 @@ namespace ClientApp.Controllers
                 return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
             }
             ViewData["categories"] = categories;
-            HttpResponseMessage productHotResponse = await Calculate.callGetApi("Product/GetProductBestSale?pageIndex=1&pageSize=4");
+            HttpResponseMessage productHotResponse = await Calculate.callGetApi("Product/GetProductHot?categoryId=" + filter.CategoryId + "&pageIndex=" + filter.pageHot + "&pageSize=4");
             if (productHotResponse.IsSuccessStatusCode)
             {
                 string results = productHotResponse.Content.ReadAsStringAsync().Result;
                 ProductPagingView productsHot = JsonConvert.DeserializeObject<ProductPagingView>(results);
                 ViewData["productsHot"] = productsHot;
-            }   
+            }
             else
             {
                 Console.WriteLine("Error Calling web API");
                 return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
             }
+            HttpResponseMessage productsBestSaleResponse = await Calculate.callGetApi("Product/GetProductBestSale?categoryId=" + filter.CategoryId + "&pageIndex=" + filter.pageSale + "&pageSize=4");
+            if (productsBestSaleResponse.IsSuccessStatusCode)
+            {
+                string results = productsBestSaleResponse.Content.ReadAsStringAsync().Result;
+                ProductPagingView productsBestSale = JsonConvert.DeserializeObject<ProductPagingView>(results);
+                ViewData["productsBestSale"] = productsBestSale;
+            }
+            else
+            {
+                Console.WriteLine("Error Calling web API");
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+
+
+
+            HttpResponseMessage productNewResponse = await Calculate.callGetApi("Product/GetProductNew?categoryId=" + filter.CategoryId + "&pageIndex=" + filter.pageNew + "&pageSize=4");
+            if (productHotResponse.IsSuccessStatusCode)
+            {
+                string results = productNewResponse.Content.ReadAsStringAsync().Result;
+                ProductPagingView productsNew = JsonConvert.DeserializeObject<ProductPagingView>(results);
+                ViewData["productsNew"] = productsNew;
+            }
+            else
+            {
+                Console.WriteLine("Error Calling web API");
+                return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            }
+            ViewData["categoryId"] = filter.CategoryId;
+            ViewData["pageHot"] = filter.pageHot;
+            ViewData["pageSale"] = filter.pageSale;
+            ViewData["pageNew"] = filter.pageNew;
             return View();
+        }
+
+        public IActionResult Filter(int id, int? pageHot = 1, int? pageSale = 1, int? pageNew = 1)
+        {
+            HomeFilterView homeFilterView = new HomeFilterView { CategoryId = id, pageHot = pageHot, pageSale = pageSale, pageNew = pageNew };
+            return RedirectToAction("Index", "Home", homeFilterView);
         }
 
         public IActionResult Privacy()
---
+++ ClientApp/Models/HomeFilterView.cs
@@ -0,0 +1,10 @@
+﻿namespace ClientApp.Models
+{
+    public class HomeFilterView
+    {
+        public int CategoryId { get; set; }
+        public int? pageHot { get; set; } = 1;
+        public int? pageNew { get; set; } = 1;
+        public int? pageSale { get; set; } = 1;
+    }
+}
